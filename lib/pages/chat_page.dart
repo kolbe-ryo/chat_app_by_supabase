1a7822fac2b5b5449622e72466f81f52734c2bd5
kolbe <water.northground@gmail.com>
2023-11-22 21:18:35 +0900

    init stream state

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app_by_supabase/model/message.dart';
+import 'package:chat_app_by_supabase/util/constants.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -14,6 +16,23 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  late final Stream<List<Message>> _messageStream;
+
+  @override
+  void initState() {
+    final userId = supabase.auth.currentUser!.id;
+    _messageStream = supabase
+        .from('message')
+        .stream(
+          primaryKey: ['id'],
+        )
+        .order('created_at')
+        .map((maps) {
+          return maps.map((map) => Message.fromMap(map: map, myUserId: userId)).toList();
+        });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
