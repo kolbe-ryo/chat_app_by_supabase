fc042bbc56fc5e88590c0838c9dfeda7c2f48e30
kolbe <water.northground@gmail.com>
2023-11-22 21:14:24 +0900

    create chat page

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      builder: (context) => const ChatPage(),
+    );
+  }
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
