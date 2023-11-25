a2e5eae3659070b53187623edb9c3f5e21e64341
kolbe <water.northground@gmail.com>
2023-11-25 22:58:46 +0900

    theme setting

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,54 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'チャットアプリ',
-      home: SplashPage(),
+      theme: ThemeData.light().copyWith(
+        primaryColorDark: Colors.orange,
+        appBarTheme: const AppBarTheme(
+          elevation: 1,
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        primaryColor: Colors.orange,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.orange,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.orange,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          floatingLabelStyle: const TextStyle(
+            color: Colors.orange,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              color: Colors.grey,
+              width: 2,
+            ),
+          ),
+          focusColor: Colors.orange,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              color: Colors.orange,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+      home: const SplashPage(),
     );
   }
 }
