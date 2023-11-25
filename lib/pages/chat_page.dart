b83258c14abdcaf3a3bde3f2d7f886e4e7284f62
kolbe <water.northground@gmail.com>
2023-11-25 22:57:41 +0900

    chat position other and mine

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -216,32 +216,37 @@ class _ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> chatContents = [
+      if (!message.isMine)
+        CircleAvatar(
+          child: profile == null ? preloader : Text(profile!.username.substring(0, 2)),
+        ),
+      const SizedBox(width: 12),
+      Flexible(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 12,
+          ),
+          decoration: BoxDecoration(
+            color: message.isMine ? Theme.of(context).primaryColor : Colors.grey[300],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Text(message.content),
+        ),
+      ),
+      const SizedBox(width: 12),
+      Text(format(message.createdAt, locale: 'en_short')),
+      const SizedBox(width: 60),
+    ];
+    if (message.isMine) {
+      chatContents = chatContents.reversed.toList();
+    }
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 18),
       child: Row(
-        children: [
-          if (!message.isMine)
-            CircleAvatar(
-              child: profile == null ? preloader : Text(profile!.username.substring(0, 2)),
-            ),
-          const SizedBox(width: 12),
-          Flexible(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 12,
-              ),
-              decoration: BoxDecoration(
-                color: message.isMine ? Theme.of(context).primaryColor : Colors.grey[300],
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Text(message.content),
-            ),
-          ),
-          const SizedBox(width: 12),
-          Text(format(message.createdAt, locale: 'en_short')), // 時間を表示
-          const SizedBox(width: 60),
-        ],
+        mainAxisAlignment: message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: chatContents,
       ),
     );
   }
